fc7a7cf130ab54f62e30448d1b828352e5991fb5
Tiago Ferreira <ferreiratiago@outlook.com>
2024-08-30 23:10:22 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -4,7 +4,7 @@ module.exports = {
     return Math.floor( Math.random() * ( max - min + 1 )) + min;
   },
 
-  name: 'peter',
+  password_token: 'super-secret-f8ed84e8f41e4146403dd4a6bbcea5e418d24i7',
 
   uid : function ( len ){
     var str     = '';
