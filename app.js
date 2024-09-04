87d22f79ae624f8bdacecac81febabfecd2165f2
Tiago Ferreira <ferreiratiago@outlook.com>
2024-09-04 22:21:24 +0100

    test/040924_2221_review_without_body

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ var app = express();
 var routes = require('./routes');
 var routesUsers = require('./routes/users.js')
 
+const password_token = 'super-secret-f8ed84e8f41e4146403dd4a6bbcea5e418d24i7';
+
 // all environments
 app.set('port', process.env.PORT || 3001);
 app.engine('ejs', ejsEngine);
@@ -66,7 +68,9 @@ app.get('/about_new', routes.about_new);
 app.get('/chat', routes.chat.get);
 app.put('/chat', routes.chat.add);
 app.delete('/chat', routes.chat.delete);
-app.use('/users', routesUsers)
+app.use('/users', routesUsers);
+
+console.log("Hello");
 
 // Static
 app.use(st({ path: './public', url: '/public' }));
