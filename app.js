4ae733aa896f917ee18a8c3487655b1c2c4372b5
Tiago Ferreira <ferreiratiago@outlook.com>
2024-11-27 15:25:36 +0000

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ const hbs = require('hbs')
 var app = express();
 var routes = require('./routes');
 var routesUsers = require('./routes/users.js')
+var sql = require('./sql_injection.js');
 
 // all environments
 app.set('port', process.env.PORT || 3001);
@@ -74,6 +75,8 @@ app.use(st({ path: './public', url: '/public' }));
 // Add the option to output (sanitized!) markdown
 marked.setOptions({ sanitize: true });
 app.locals.marked = marked;
+
+checkLogin();
 
 // development only
 if (app.get('env') == 'development') {
