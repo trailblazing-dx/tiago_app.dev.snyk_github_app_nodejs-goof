04139cd3822212a0fde4fbecaebdb75e1b2a505d
Tiago Ferreira <ferreiratiago@outlook.com>
2025-02-05 22:01:53 +0000

    Update sql_injection.js

--- sql_injection.js
+++ sql_injection.js
@@ -1,7 +1,7 @@
 const Utils = require("./utils")
 
 function checkLogin(req, db) {
-  const sqlQuery = Utils.getQuery(req.body.email);
+  const sqlQuery = Utils.getQuery(req);
 
     doQuery(sqlQuery, [])
 }
