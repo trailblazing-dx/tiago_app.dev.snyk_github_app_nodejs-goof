29feb321ef61b85c24990c73c7352085dbff02b9
Ashok Narayanan <ashok.narayanan@snyk.io>
2025-03-17 18:47:21 -0500

    Create TODO for login handler

--- routes/index.js
+++ routes/index.js
@@ -35,23 +35,7 @@ exports.index = function (req, res, next) {
 };
 
 exports.loginHandler = function (req, res, next) {
-	// CODE below has vulnerabilities
-  /*
-  if (validator.isEmail(req.body.username)) {
-    User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
-      if (users.length > 0) {
-        const redirectPage = req.body.redirectPage
-        const session = req.session
-        const username = req.body.username
-        return adminLoginSuccess(redirectPage, session, username, res)
-      } else {
-        return res.status(401).send()
-      }
-    });
-  } else {
-    return res.status(401).send()
-  }
-  */
+  // TODO Implement a login handler here
 };
 
 function adminLoginSuccess(redirectPage, session, username, res) {
